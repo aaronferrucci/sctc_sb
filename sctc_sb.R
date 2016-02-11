0ca3cb90c04a1354f67d757d33de2f6e18a5096e
Aaron Ferrucci <aaronf@antfarm.org>
2016-02-10 21:25:00 -0800

    More plots, more words.

--- sctc_sb.R
+++ sctc_sb.R
@@ -1,29 +1,30 @@
 source("sctc_sb_utils.R")
+
+get_plot <- function(data, max_age) {
+  y_values <- seq(0, max(data$Time), 600)
+  plot <- ggplot(data, aes(x = Age, y = Time, color = Gender)) +
+    scale_y_continuous(breaks = y_values, labels = timestr(y_values)) +
+    scale_x_continuous(breaks = seq(0, max_age, 10)) +
+    geom_point() +
+    stat_smooth(formula = y ~ x)
+
+  return(plot)
+}
+
 year <- 2016
 the_data <- get_data_from_url(year)
 data3k <- the_data[[1]]
 data10k <- the_data[[2]]
-
-y_values_3k <- seq(0, max(data3k$Time), 600)
-
 max_age <- max(data3k$Age, data10k$Age)
 
-plot3k <-
-  ggplot(data3k, aes(x = Age, y = Time, color = Gender)) +
-    scale_y_continuous(breaks = y_values_3k, labels = timestr(y_values_3k)) +
-    scale_x_continuous(breaks = seq(0, max_age, 10)) +
-    ggtitle("3k") +
-    geom_point() +
-    stat_smooth(formula = y ~ x)
-  
-y_values_10k <- seq(0, max(data10k$Time), 600)
+get_age_histogram <- function(data) {
+  plot <- ggplot(data, aes(x = Age, fill = Gender), xlab="Age") +
+    geom_histogram(binwidth=5) +
+    ggtitle("10k Age histogram") +
+    scale_x_continuous(breaks=seq(0, max(data$Age), 10)) +
+    geom_vline(xintercept = mean(data$Age))
 
-plot10k <- ggplot(data10k, aes(x = Age, y = Time, color = Gender)) + 
-  scale_y_continuous(breaks = y_values_10k, labels = timestr(y_values_10k)) +
-  scale_x_continuous(breaks = seq(0, max_age, 10)) +
-  ggtitle("10k") +
-  geom_point() +
-  stat_smooth(formula = y ~ x)
+  return(plot)
+}
 
 
-grid.arrange(plot3k, plot10k, nrow=2, top=paste(year, "SCTC Super Bowl 3k/10k"))
